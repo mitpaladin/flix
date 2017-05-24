b36d675fdb3373f567d100ab325163f2d900dc1c
Jeff Dickey <jdickey@seven-sigma.com>
2017-05-25 02:08:06 +0800

    As after '3.3 Home Page', with changes.
    Changes needed because the [tutorial](https://www.sigient.com/blog/movie-listings-application-with-react-router-v-4) which this repo walks through **does not** use the actually-released React Router v4, but instead documents a (fairly early) prerelease, and so numerous changes occurred between that time and this. This will confuse the heck out of a novice to React who's simply trying to wrap her head around routing.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import Home from './components/Home';
+
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+// import Router from 'react-router/BrowserRouter';
+// import Match from 'react-router/Match';
+// import Link from 'react-router/Link';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Flix</h2>
+      <Router>
+        <div className="App">
+          <div className="App-header">
+            <h2>Flix</h2>
+            <Link to="/">Home</Link>
+          </div>
+
+          <div className="container">
+            <Route path="/" exact component={Home} />
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
