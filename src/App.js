4211cbcc53432cda1b88c70e36a0420ce8b7a058
Jeff Dickey <jdickey@seven-sigma.com>
2017-05-25 20:26:03 +0800

    After '3.5 Movies Page'

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
 
 import Home from './components/Home';
 import Movie from './components/Movie';
+import Movies from './components/Movies';
 
 class App extends Component {
   render() {
@@ -15,11 +16,13 @@ class App extends Component {
           <div className="App-header">
             <h2>Flix</h2>
             <Link to="/">Home</Link>
+            <Link to="/movies">Movies</Link>
           </div>
 
           <div className="container">
             <Switch>
               <Route path="/" exact component={Home} />
+              <Route path="/movies" exact component={Movies} />
               <Route path="/movies/:movieId" component={Movie} />
             </Switch>
           </div>
