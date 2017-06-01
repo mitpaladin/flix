8b50afd032e37e77b4e52bf7339333c05e6c5bcd
Jeff Dickey <jdickey@seven-sigma.com>
2017-06-01 14:31:45 +0800

    Add to MovieMetadata a comment on incomplete semantics of SUI List items

--- src/components/Movie/Metadata.js
+++ src/components/Movie/Metadata.js
@@ -6,6 +6,10 @@ import { Container } from 'semantic-ui-react';
 
 import MoviePropTypes from './PropTypes';
 
+/* Oddly enough, Semantic UI has no direct analogue to a definition list; the
+   closest they come is where their Item can contain a [Header](https://semantic-ui.com/elements/list#header),
+   which isn't the same at all if you think about it. Pffft. */
+
 const MovieMetadata = ({ movie, className }) => {
   return (
     <Container className={className}>
