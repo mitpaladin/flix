fec64950874312a9d537640f6576ce38409d7dbe
Jeff Dickey <jdickey@seven-sigma.com>
2017-06-04 14:54:03 +0800

    Extract styling for MovieReviews component

--- src/components/Movie/Reviews.js
+++ src/components/Movie/Reviews.js
@@ -7,20 +7,22 @@ import { Container, Divider, Header } from 'semantic-ui-react';
 import MovieReview from './Review';
 import { MoviePropTypes } from './PropTypes';
 
-const MovieReviews = ({ reviews }) => {
+import './Reviews.css';
+
+const MovieReviews = (props) => {
   const keyFor = (review) => {
     return([review.title, review.author].join('-'));
   };
 
-  const items = reviews.map((review) => {
+  const items = props.reviews.map((review) => {
     return(
       <MovieReview key={keyFor(review)} review={review} />
     );
   });
 
   return (
-    <Container className="movie-reviews">
-      <Header as="h2">Reviews</Header>
+    <Container className={props.className}>
+      <Header>Reviews</Header>
       <Divider section><hr /></Divider>
       {items}
     </Container>
@@ -28,7 +30,12 @@ const MovieReviews = ({ reviews }) => {
 };
 
 MovieReviews.propTypes = {
+  className: PropTypes.string,
   reviews: PropTypes.arrayOf(MoviePropTypes.Review).isRequired,
+}
+
+MovieReviews.defaultProps = {
+  className: "movie-reviews",
 }
 
 export default MovieReviews;
